189cdfcfcf57fc7ce213b36241e9baff5f8a4d56
Ahmed <ahmedfaaid.coding@gmail.com>
2020-02-13 21:35:53 -0500

    Reduce round trip queries with dataloader

--- helpers/index.js
+++ helpers/index.js
@@ -1,5 +1,14 @@
+const DataLoader = require('dataloader')
 const Event = require('../models/event')
 const User = require('../models/user')
+
+const eventLoader = new DataLoader(eventIds => {
+    return events(eventIds)
+})
+
+const userLoader = new DataLoader(userIds => {
+    return User.find({ _id: { $in: userIds } })
+})
 
 const events = async eventIds => {
     try {
@@ -14,8 +23,8 @@ const events = async eventIds => {
 
 const singleEvent = async eventId => {
     try {
-        const event = await Event.findById(eventId)
-        return eventTransformer(event)
+        const event = await eventLoader.load(eventId.toString())
+        return event
     } catch (err) {
         throw err
     }
@@ -23,10 +32,10 @@ const singleEvent = async eventId => {
 
 const user = async userId => {
     try {
-        const user = await User.findById(userId)
+        const user = await userLoader.load(userId.toString())
         return {
             ...user._doc,
-            createdEvents: events.bind(this, user._doc.createdEvents)
+            createdEvents: eventLoader.load.bind(this, user._doc.createdEvents)
         }
     } catch (err) {
         throw err
